b08766aa46d6840ae560ae0f2d3ed64cf53249b8
vakhimDev <vakhimnuon@gmail.com>
2026-01-22 16:27:15 +0700

    first init

---
+++ src/test/java/com/banking/java_banking/JavaBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.java_banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
